d30bc650a661269ba6248b61782bceaaf2cc656a
RyanStLouis <ryan.stlouis@ocsbstudent.ca>
2015-11-18 14:29:14 -0500

    hi

---
+++ Marks/Marks.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Marks
+{
+    partial class frmMarks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.lblEnter = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.txtEnter = new System.Windows.Forms.TextBox();
+            this.lstMarks = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(12, 180);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 0;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(197, 180);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(75, 23);
+            this.btnAverage.TabIndex = 1;
+            this.btnAverage.Text = "Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // lblEnter
+            // 
+            this.lblEnter.AutoSize = true;
+            this.lblEnter.Location = new System.Drawing.Point(12, 27);
+            this.lblEnter.Name = "lblEnter";
+            this.lblEnter.Size = new System.Drawing.Size(246, 13);
+            this.lblEnter.TabIndex = 2;
+            this.lblEnter.Text = "Please enter each mark and click the enter button.";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(12, 219);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(81, 13);
+            this.lblAverage.TabIndex = 3;
+            this.lblAverage.Text = "The average is:";
+            // 
+            // txtEnter
+            // 
+            this.txtEnter.Location = new System.Drawing.Point(15, 71);
+            this.txtEnter.Name = "txtEnter";
+            this.txtEnter.Size = new System.Drawing.Size(100, 20);
+            this.txtEnter.TabIndex = 4;
+            // 
+            // lstMarks
+            // 
+            this.lstMarks.FormattingEnabled = true;
+            this.lstMarks.Location = new System.Drawing.Point(152, 71);
+            this.lstMarks.Name = "lstMarks";
+            this.lstMarks.Size = new System.Drawing.Size(120, 95);
+            this.lstMarks.TabIndex = 5;
+            // 
+            // frmMarks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lstMarks);
+            this.Controls.Add(this.txtEnter);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lblEnter);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.btnEnter);
+            this.Name = "frmMarks";
+            this.Text = "Marks";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Label lblEnter;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.TextBox txtEnter;
+        private System.Windows.Forms.ListBox lstMarks;
+    }
+}
+
---
+++ Marks/Marks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Marks
+{
+    public partial class frmMarks : Form
+    {
+        int total = 0;
+        int numberOfNumbers = 0;
+        public frmMarks()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            numberOfNumbers = numberOfNumbers + 1;
+            total = total + Convert.ToInt32(txtEnter.Text);
+            lstMarks.Items.Add(Convert.ToInt32(txtEnter.Text));
+        }
+
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+            this.lblAverage.Text = "The Average is: " + Convert.ToString(total / numberOfNumbers);
+        }
+    }
+}
